842078e77b9efe83c68b216442e7d86c604c4dd3
Isaac Houston <isaacprh@gmail.com>
2020-10-21 11:58:06 -0300

    Estilizando PagePreview

--- src/components/PagePreview/pagePreview.tsx
+++ src/components/PagePreview/pagePreview.tsx
@@ -23,42 +23,52 @@ const PagePreview: React.FC<IPageData> = (props ) => {
   }, [props.styleConfig]);
 
   return (
-  <Grid className="preview" container sm={12}>
+    
+    <Grid container xl className="container">    
 
-    <Grid item className="text-top">
-    <h1>Grid Direita</h1>
-    </Grid>    
-    <Grid item className="background-logo">
-      <img className="logo-img" alt="[]" src={props.styleConfig.topBackgroundUrl}/>
-    </Grid>    
-        <Paper className="">
-          <Grid container spacing={2} >
-           
-            <Grid item xs={12} sm container>
-              <Grid item xs container direction="column" spacing={2}>
-                <Grid item xs className="card">
+      <Grid className="preview-top" item sm={12}>
 
-                  <img className="card-img" alt="[]" src={props.styleConfig.logoUrl}/>
-                  <Grid item className="text-card">
-                  <Typography gutterBottom variant="subtitle1">
-                    Filé com Fritas
-                    </Typography>
-                  <Typography variant="body2" gutterBottom>
-                  300g de Filé - 400g de Batata Frita
+      <Grid item className="text-top">
+      <h1>Preview Layout</h1>
+      </Grid>    
+        <Grid item  className="div-top">
+          
+            <Grid item className="background-top">
+              <img className="background-top-img" alt="[Imagem do banner]" src={props.styleConfig.topBackgroundUrl}/>
+              <Grid item className="logo">
+                <img className="logo-img" alt="[Imagem da logo]" src={props.styleConfig.logoUrl}/>
+              </Grid>    
+            </Grid> 
+        </Grid> 
+          <Paper className="preview-card">
+            <Grid container spacing={2} >           
+              <Grid item xs={12} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                  <Grid item xs className="card">
+                    <img className="card-img" alt="[]" src={props.styleConfig.logoUrl}/>
+                    <Grid item className="text-card">
+                    <Typography gutterBottom variant="subtitle1">
+                      Filé com Fritas
+                      </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      300g de Filé - 400g de Batata Frita
                     </Typography>    
-                  </Grid>
-                    
-                </Grid>              
-              </Grid>
-              <Grid item>
-                <Typography variant="subtitle1">R$50.00</Typography>
+                    </Grid>                    
+                  </Grid>              
+                </Grid>
+                    <Grid item>
+                      <Typography variant="subtitle1">R$50.00</Typography>
+                    </Grid>
               </Grid>
             </Grid>
-          </Grid>
-        </Paper>
-
-   
-  </Grid>
+          </Paper>
+        </Grid>
+        <Grid item className="footer">
+            <h2>Ver carrinho</h2>
+        </Grid>
+      </Grid>
+    
+      
   );
 }
 
